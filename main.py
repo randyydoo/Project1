68ddf5413f187023f8512d1e0ad7d9681a2111a1
randyydoo <randyydoo@gmail.com>
2023-10-21 18:05:21 -0700

    added float to time conversion function

--- main.py
+++ main.py
@@ -1,11 +1,19 @@
-def to_float(time: str) -> float:
+def toFloat(time: str) -> float:
     time = time.replace(':', '.')
     return float(time)
 
+def floatToTime(f: float) -> float:
+    decimal = f - int(f)
+    if decimal > .60:
+        remainder = decimal - .60
+        return (1 + int(f)) + remainder
+
+    return f
+
 def greedy(input1: list[list[str, str]], act1: list[str,str], input2: list[list[str,str]], act2: list[str, str]) -> list[list[str, str]]:
     # check if daily activties have an open interval
-    act1[0], act1[1] = to_float(act1[0]), to_float(act1[1])
-    act2[0], act2[1] = to_float(act2[0]), to_float(act2[1])
+    act1[0], act1[1] = toFloat(act1[0]), toFloat(act1[1])
+    act2[0], act2[1] = toFloat(act2[0]), toFloat(act2[1])
 
     if act1[1] < act2[0] or act2[1] < act1[0]:
         return []
@@ -16,12 +24,12 @@ def greedy(input1: list[list[str, str]], act1: list[str,str], input2: list[list[
     # replace all values in inputs to floats for comparison
     for i in range(len(input1)):
         start, end = input1[i][0], input1[i][1]
-        start, end = to_float(start), to_float(end)
+        start, end = toFloat(start), toFloat(end)
         input1[i][0], input1[i][1] = start, end
 
     for i in range(len(input2)):
         start, end = input2[i][0], input2[i][1]
-        start, end = to_float(start), to_float(end)
+        start, end = toFloat(start), toFloat(end)
         input2[i][0], input2[i][1] = start, end
     
 
@@ -29,24 +37,16 @@ def greedy(input1: list[list[str, str]], act1: list[str,str], input2: list[list[
     input1 = sorted(input1, key=lambda x: x[1])
     input2 = sorted(input2, key=lambda x: x[1])
     
+    
+    # find avaliable times in input1 and compare with input2
+    start, end = interval[0], interval[0], interval[1]
+
+
     duration = float(duration)
 
-    ans = []
-    curr, start, end = interval[0], interval[0], interval[1]
 
-    # loop through smaller list and 
-    length = min(len(input1), len(input2))
-        # take max of curr end
-        # take min of next start time
-        if minute 
-
-    print(input1, '\n', act1, '\n', input2, '\n', act1)
      
 greedy([['16:00', '18:00'], ['7:00', '8:30'], ['12:00', '13:00']], ['9:00', '19:00'], [['9:00', '10:30'], ['16:00', '17:00'], ['12:20', '13:30'], ['14:00', '15:00']], ['9:00', '18:30'])
-
-
-
-
 
 
 
